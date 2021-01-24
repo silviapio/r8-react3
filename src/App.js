cc99f1382406a880df811b6aabd6475d2e470ca7
silviapio <silvia.piovesan@gmail.com>
2021-01-24 17:48:52 +0100

    working API call with axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Joke from "./Joke";
+
 export default () => {
+    const [currentPosition, setCurrentPosition] = useState(-1);
+    const [jokes, setJokes] = useState([]);
+    const currentJoke = jokes[currentPosition];
+
+    useEffect(() => {
+        if (currentPosition === jokes.length) {
+        axios.get("https://icanhazdadjoke.com/", { 
+            headers: {
+                "Accept": "application/json"
+            }
+        }).then((response) => {
+            setJokes([
+                ...jokes,
+                response.data.joke
+            ]);
+        });
+    } 
+    }, [currentPosition])
+
+
+    function handleClick(increment) {
+        setCurrentPosition(prevPosition => prevPosition + increment);
+        
+    }
+    /*function increment() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    function decrement() {
+        setCount(prevCount => prevCount - 1);
+    }*/
 
     return (
         <div>
-            hello World!
+            <header>Let's begin the day with a good laugh!</header>
+            <Joke text={currentJoke} />
+            <p>{/*currentPosition*/}</p>
+            <div>
+                <button onClick={() => handleClick(-1)}>Previous</button>
+                <button onClick={() => handleClick(+1)}>Next</button>
+            </div>
         </div>
     );
 };
