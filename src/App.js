fef57ddf8e5eac7d17a3c01df05baa7585ae76b9
silviapio <silvia.piovesan@gmail.com>
2021-01-25 12:13:40 +0100

    async fetch working; change of logic with NEXT button only

--- src/App.js
+++ src/App.js
@@ -5,44 +5,32 @@ import Joke from "./Joke";
 export default () => {
     const [currentPosition, setCurrentPosition] = useState(-1);
     const [jokes, setJokes] = useState([]);
-    const currentJoke = jokes[currentPosition];
+    let currentJoke = jokes[currentPosition];
 
-    useEffect(() => {
-        if (currentPosition === jokes.length) {
-        axios.get("https://icanhazdadjoke.com/", { 
-            headers: {
-                "Accept": "application/json"
-            }
-        }).then((response) => {
-            setJokes([
-                ...jokes,
-                response.data.joke
-            ]);
-        });
-    } 
-    }, [currentPosition])
-
-
-    function handleClick(increment) {
-        setCurrentPosition(prevPosition => prevPosition + increment);
-        
+    async function handleClick() {
+        if (currentPosition < jokes.length) {
+            let newIndex = await axios.get("https://icanhazdadjoke.com/", {
+                headers: {
+                    "Accept": "application/json"
+                }
+            }).then((response) => {
+                setJokes([
+                    ...jokes,
+                    response.data.joke
+                ]);
+                return jokes.length;
+            });
+            setCurrentPosition(newIndex);   
+        }             
     }
-    /*function increment() {
-        setCount(prevCount => prevCount + 1);
-    }
-
-    function decrement() {
-        setCount(prevCount => prevCount - 1);
-    }*/
 
     return (
         <div>
             <header>Let's begin the day with a good laugh!</header>
             <Joke text={currentJoke} />
-            <p>{/*currentPosition*/}</p>
+            <p>{currentPosition}</p>
             <div>
-                <button onClick={() => handleClick(-1)}>Previous</button>
-                <button onClick={() => handleClick(+1)}>Next</button>
+                <button onClick={() => handleClick()}>Next</button>
             </div>
         </div>
     );
