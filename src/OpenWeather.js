4b9252989ac1ff9f2b88463141463e2a10077707
silviapio <silvia.piovesan@gmail.com>
2021-01-28 15:00:16 +0100

    fix api key exposure

--- src/OpenWeather.js
+++ src/OpenWeather.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import weatherID from './privateKeys.js';
 
 export default (props) => {
     const [ weatherText, setWeatherText ] = useState("");
-    const weatherID = "500a5cfb8db5ea91e3d49a9f8bee71c0";
     
     useEffect(() => {
         axios.get("http://api.openweathermap.org/data/2.5/weather" , {
